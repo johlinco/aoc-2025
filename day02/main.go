82bdd0bfb72ac564ce5798ddfff319c7dbcc7072
Lincoln Hoey Moore <47604064+johlinco@users.noreply.github.com>
2025-12-02 15:16:48 -0500

    Day 2 solutions

--- day02/main.go
+++ day02/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -17,8 +18,8 @@ func main() {
 		panic(err)
 	}
 
-	exampleLines := strings.Split(strings.TrimSpace(string(example)), "\n")
-	inputLines := strings.Split(strings.TrimSpace(string(input)), "\n")
+	exampleLines := strings.Split(strings.TrimSpace(string(example)), ",")
+	inputLines := strings.Split(strings.TrimSpace(string(input)), ",")
 
 	// Part 1 Example
 	part1Example := solvePart1(exampleLines)
@@ -38,11 +39,89 @@ func main() {
 }
 
 func solvePart1(lines []string) int {
-	// TODO: Implement part 1
-	return 0
+	sumInvalidIDs := 0
+
+	for _, line := range lines {
+		startStr := strings.Split(line, "-")[0]
+		start, err := strconv.Atoi(startStr)
+		if err != nil {
+			fmt.Printf("start num %s is invalid\n", startStr)
+		}
+		endStr := strings.Split(line, "-")[1]
+		end, err := strconv.Atoi(endStr)
+		if err != nil {
+			fmt.Printf("end num %s is invalid\n", endStr)
+		}
+		//fmt.Printf("start is %d and end is %d\n", start, end)
+		for num := start; num <= end; num++ {
+			numString := strconv.Itoa(num)
+			if firstHalfEqualsSecondHalf(numString) {
+				//fmt.Printf("adding %d to sum\n", num)
+				sumInvalidIDs += num
+			}
+		}
+	}
+	return sumInvalidIDs
 }
 
 func solvePart2(lines []string) int {
-	// TODO: Implement part 2
-	return 0
+	sumInvalidIDs := 0
+
+	for _, line := range lines {
+		startStr := strings.Split(line, "-")[0]
+		start, err := strconv.Atoi(startStr)
+		if err != nil {
+			fmt.Printf("start num %s is invalid\n", startStr)
+		}
+		endStr := strings.Split(line, "-")[1]
+		end, err := strconv.Atoi(endStr)
+		if err != nil {
+			fmt.Printf("end num %s is invalid\n", endStr)
+		}
+		//fmt.Printf("start is %d and end is %d\n", start, end)
+		for num := start; num <= end; num++ {
+			numString := strconv.Itoa(num)
+			if isMadeOfRepeats(numString) {
+				//fmt.Printf("adding %d to sum\n", num)
+				sumInvalidIDs += num
+			}
+		}
+	}
+	return sumInvalidIDs
+}
+
+func firstHalfEqualsSecondHalf(string string) bool {
+	length := len(string)
+	if length%2 != 0 {
+		return false
+	}
+
+	mid := length / 2
+
+	firstHalf := string[:mid]
+	secondHalf := string[mid:]
+
+	return firstHalf == secondHalf
+}
+
+func isMadeOfRepeats(string string) bool {
+	length := len(string)
+
+	breakPoint := length / 2
+
+	for breakPoint > 0 {
+		if length%breakPoint == 0 {
+			firstSection := string[0:breakPoint]
+			for i := breakPoint; i < length; i += breakPoint {
+				if string[i:i+breakPoint] != firstSection {
+					break
+				}
+				if i+breakPoint == length {
+					return true
+				}
+			}
+		}
+		breakPoint--
+	}
+	return false
 }
--- day02/main_test.go
+++ day02/main_test.go
@@ -12,10 +12,10 @@ func TestPart1Example(t *testing.T) {
 		t.Fatalf("failed to read example: %v", err)
 	}
 
-	lines := strings.Split(strings.TrimSpace(string(input)), "\n")
+	lines := strings.Split(strings.TrimSpace(string(input)), ",")
 	result := solvePart1(lines)
-	
-	expected := 0 // TODO: Update with expected value
+
+	expected := 1227775554
 	if result != expected {
 		t.Errorf("Part 1 = %d; want %d", result, expected)
 	}
@@ -27,10 +27,10 @@ func TestPart2Example(t *testing.T) {
 		t.Fatalf("failed to read example: %v", err)
 	}
 
-	lines := strings.Split(strings.TrimSpace(string(input)), "\n")
+	lines := strings.Split(strings.TrimSpace(string(input)), ",")
 	result := solvePart2(lines)
-	
-	expected := 0 // TODO: Update with expected value
+
+	expected := 4174379265
 	if result != expected {
 		t.Errorf("Part 2 = %d; want %d", result, expected)
 	}
@@ -39,7 +39,7 @@ func TestPart2Example(t *testing.T) {
 func BenchmarkPart1(b *testing.B) {
 	input, _ := os.ReadFile("input.txt")
 	lines := strings.Split(strings.TrimSpace(string(input)), "\n")
-	
+
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		solvePart1(lines)
@@ -49,7 +49,7 @@ func BenchmarkPart1(b *testing.B) {
 func BenchmarkPart2(b *testing.B) {
 	input, _ := os.ReadFile("input.txt")
 	lines := strings.Split(strings.TrimSpace(string(input)), "\n")
-	
+
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		solvePart2(lines)
